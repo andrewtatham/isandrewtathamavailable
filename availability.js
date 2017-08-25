f918e1a7001c084e40563fd75f09231aa42b6d52
Andrew Tatham <andrewtatham@gmail.com>
2017-08-25 17:36:11 +0100

    Revert "availability"
    This reverts commit 6d98f5300be04bac4fd2acbbfdb49833cf323003.

--- availability.js
+++ availability.js
@@ -1,3 +1,3 @@
 var isAvailable = false;
-var inContractUntil = "the end of August";
-var availableFrom = "the end of August";
+var inContractUntil = "the end of September";
+var availableFrom = "the end of September";
