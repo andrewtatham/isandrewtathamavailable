94e7358adf30862d9472b8833111ae026ad4e40c
Andrew Tatham <andrewtatham@gmail.com>
2018-01-18 17:03:00 +0000

    availability

--- availability.js
+++ availability.js
@@ -1,3 +1,3 @@
 var isAvailable = false;
-var inContractUntil = "the end of January 2018 (and likely to renew)";
-var availableFrom = "the end of January 2018";
+var inContractUntil = "the end of February (and likely to renew)";
+var availableFrom = "the end of February";
