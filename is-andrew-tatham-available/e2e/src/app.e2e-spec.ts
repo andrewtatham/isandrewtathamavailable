7169e35bfd196345718c04f3b9e20b0f16bfcf0b
Andrew Tatham <andrewtatham@gmail.com>
2018-06-11 10:19:41 +0100

    angular tests fixed

--- is-andrew-tatham-available/e2e/src/app.e2e-spec.ts
+++ is-andrew-tatham-available/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to is-andrew-tatham-available!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
