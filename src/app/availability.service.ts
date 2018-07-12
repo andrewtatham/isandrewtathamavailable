5bd3f35157e4f2809edfbf62fac869b22ed3ae95
Andrew Tatham <andrewtatham@gmail.com>
2018-07-12 21:16:17 +0100

    availability

--- src/app/availability.service.ts
+++ src/app/availability.service.ts
@@ -8,8 +8,8 @@ export class AvailabilityService {
   constructor() { }
   getAvailability(): Availability {
     let isAvailable = false;
-    let availableText = "I'm available immediately";
-    let unavailableText = "I'm not available right now.";
+    let availableText = "I'm available immediately.";
+    let unavailableText = "I'm not available. I'm in contract until the end of August. Please do not call.";
 
     let availability = new Availability(
       isAvailable,
