e33be238721be7c5ca5047d06a34a0099218ae38
Andrew Tatham <andrewtatham@gmail.com>
2019-01-14 09:33:24 +0000

    publish to aws

--- publish.cmd
+++ publish.cmd
@@ -1,1 +1,2 @@
+aws s3 rm "s3://isandrewtathamavailable.com/" --recursive --exclude "*.docx"
 aws s3 cp "dist\is-andrew-tatham-available" "s3://isandrewtathamavailable.com/" --recursive --include "*" --acl public-read
