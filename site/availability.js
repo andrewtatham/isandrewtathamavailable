557881d759be9f0eb93895cf6f19bb7fe96d5b5d
Andrew Tatham <andrewtatham@gmail.com>
2019-04-12 09:55:24 +0100

    availability

--- site/availability.js
+++ site/availability.js
@@ -1,4 +1,4 @@
 'use strict';
-var isAvailable = true;
-var inContractUntil = " No, I'm not available. I'm in contract until [date].";
+var isAvailable = false;
+var inContractUntil = " I'm <b>not</b> available.<br> I'm in contract until the end of July.<br>Thanks.";
 var availableFrom = " Yes. I'm available immediately.";
